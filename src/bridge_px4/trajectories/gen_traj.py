e8a22f45efe5f29a7c5e0ef1403746b4286dfa1a
Rianna Jitosho <rjitosho@stanford.edu>
2023-12-06 11:55:29 -0800

    Add preland sequence

--- src/bridge_px4/trajectories/gen_traj.py
+++ src/bridge_px4/trajectories/gen_traj.py
@@ -4,15 +4,21 @@ import matplotlib.pyplot as plt
 
 
 # Generate a trajectory
-T = 10.0
+T = 100.0
 t = np.arange(0.0, T, 0.05)
 
-x = np.sin(2*np.pi*t*2/T)
-y = np.cos(2*np.pi*t*3/T)
-z = np.ones(len(t))
+# sinusoids
+# x = np.sin(2*np.pi*t*2/T)
+# y = np.cos(2*np.pi*t*3/T)
+# z = np.ones(len(t))
+
+# vine preland - go diagonally in x and z
+x = np.minimum(.3*t, 1.2*np.ones(len(t)))
+y = np.zeros(len(t))
+z = np.maximum(1.5-.3*t, 0.3*np.ones(len(t)))
 
 # plot trajectory
-plt.plot(x,y)
+plt.plot(x,z)
 plt.axis('equal')
 plt.show()
 
@@ -25,4 +31,4 @@ X[3,:] = z
 X[7,:] = np.ones(len(t))
 
 # save trajectory
-np.savetxt('test.csv', X, delimiter=',', fmt='%1.3f')
+np.savetxt('preland.csv', X, delimiter=',', fmt='%1.3f')
