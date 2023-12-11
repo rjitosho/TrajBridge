f6360af62cd31a8df1615a8636e6a6a9fb0782f8
Rianna Jitosho <rjitosho@stanford.edu>
2023-12-10 21:24:53 -0800

    Add vine bridge and keyboard nodes

---
+++ src/bridge_px4/src/gcs_pressure.py
@@ -0,0 +1,25 @@
+# #!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def main():
+    rospy.init_node('teensy_communication_python')
+    pub = rospy.Publisher('pressure_cmd', String, queue_size=1)
+
+    while not rospy.is_shutdown():
+        # request body pressure
+        input_str = input("Enter body pressure: ")
+        try:
+            _ = float(input_str)
+        except ValueError:
+            continue
+        
+        pub.publish(input_str)
+        rospy.sleep(0.1)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
