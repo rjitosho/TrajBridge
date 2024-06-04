bc1d1029a6b3e2afe35ad9dedc0f1696da86451d
Rianna Jitosho <rjitosho@stanford.edu>
2024-06-04 01:15:48 -0700

    0604

--- src/bridge_px4/trajectories/traj_manual_AY.py
+++ src/bridge_px4/trajectories/traj_manual_AY.py
@@ -2,6 +2,15 @@ import numpy as np
 from numpy import savetxt
 import matplotlib.pyplot as plt
 from scipy import signal
+from scipy.io import loadmat
+
+u = np.array(loadmat('/home/oem/StanfordMSL/TrajBridge/src/bridge_px4/trajectories/Test_0604.mat')['u_opt'])
+print(u.shape)
+
+t = np.arange(159) * 0.05
+x = u[0,:]
+y = u[1,:]
+z = u[2,:]
 
 def time(t0, t1):
     return np.arange(t0, t1, 0.05)
@@ -15,25 +24,25 @@ def traj_u_line(t0, t1, x0, x1):
     len = time(t0, t1).shape[0]
     return np.linspace(x0, x1, len)
 
-x0 = traj_u_step(0, 5, 0)
-x1 = traj_u_line(5, 5.5, 0, 1.5)
-x2 = traj_u_step(5.5, 5.75, 1.5)
-x3 = traj_u_line(5.75, 6.75, 1.5, -1.5)
-x4 = traj_u_step(6.75, 7.25, -1.5)
-x5 = traj_u_line(7.25, 13, -1.5, 0)
-x6 = traj_u_step(13, 17, 0)
-x = np.concatenate((x0, x1, x2, x3, x4, x5, x6))
-t = time(0, 17)
+# x0 = traj_u_step(0, 5, 0)
+# x1 = traj_u_line(5, 5.5, 0, 1.5)
+# x2 = traj_u_step(5.5, 5.75, 1.5)
+# x3 = traj_u_line(5.75, 6.75, 1.5, -1.5)
+# x4 = traj_u_step(6.75, 7.25, -1.5)
+# x5 = traj_u_line(7.25, 13, -1.5, 0)
+# x6 = traj_u_step(13, 17, 0)
+# x = np.concatenate((x0, x1, x2, x3, x4, x5, x6))
+# t = time(0, 17)
 
-y = np.zeros(len(t))
-z0 = traj_u_step(0, 5, 1.75)
-z1 = traj_u_line(5, 5.5, 1.75, 1.6)
-z2 = traj_u_step(5.5, 5.75, 1.6)
-z3 = traj_u_line(5.75, 6.75, 1.6, 1.95)
-z4 = traj_u_step(6.75, 7.25, 1.95)
-z5 = traj_u_line(7.25, 13, 1.95, 1.75)
-z6 = traj_u_step(13, 17, 1.75)
-z = np.concatenate((z0, z1, z2, z3, z4, z5, z6))
+# y = np.zeros(len(t))
+# z0 = traj_u_step(0, 5, 1.75)
+# z1 = traj_u_line(5, 5.5, 1.75, 1.6)
+# z2 = traj_u_step(5.5, 5.75, 1.6)
+# z3 = traj_u_line(5.75, 6.75, 1.6, 1.95)
+# z4 = traj_u_step(6.75, 7.25, 1.95)
+# z5 = traj_u_line(7.25, 13, 1.95, 1.75)
+# z6 = traj_u_step(13, 17, 1.75)
+# z = np.concatenate((z0, z1, z2, z3, z4, z5, z6))
 
 # z = 1.75*np.ones(len(t))
 
@@ -55,4 +64,4 @@ plt.show()
 # plt.show()
 
 # save trajectory
-np.savetxt('/home/oem/StanfordMSL/TrajBridge/src/bridge_px4/trajectories/Traj0602_testwithz.csv', X, delimiter=',', fmt='%1.3f')
+np.savetxt('/home/oem/StanfordMSL/TrajBridge/src/bridge_px4/trajectories/Traj0604_test.csv', X, delimiter=',', fmt='%1.3f')
